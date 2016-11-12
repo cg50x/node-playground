c084d471bb5230c629545ba5640a24b5d79fe14c
Chris Guevara <chrismguevara@gmail.com>
2016-11-12 12:55:48 -0800

    Implement KthToLast and Remove duplicates

--- linked-list/KthToLast.js
+++ linked-list/KthToLast.js
@@ -1,3 +1,33 @@
 /*
 	Implement an algorithm to find the kth to last element of a singly linked list.
 */
+const SinglyLinkedList = require('./SinglyLinkedList');
+
+// Will k be greater than the length of the list?
+// If k is greater than the list, return null
+function findKthToLastElement(headNode, k) {
+	// Set runner node to the kth element from the beginning
+	let runnerNode = headNode;
+	for (let i = 0; i < k && runnerNode !== null; i++) {
+		runnerNode = runnerNode.next;
+	}
+	// If runner node is null, return null
+	if (runnerNode === null) {
+		return null;
+	}
+	let currentNode = headNode;
+	while (runnerNode !== null) {
+		runnerNode = runnerNode.next;
+		currentNode = currentNode.next;
+	}
+	return currentNode.value;
+}
+
+let linkedList = new SinglyLinkedList();
+linkedList.add(1);
+linkedList.add(2);
+linkedList.add(3);
+linkedList.add(4);
+linkedList.add(5);
+linkedList.add(6);
+console.log(findKthToLastElement(linkedList._head, 3));
--- linked-list/RemoveDuplicates.js
+++ linked-list/RemoveDuplicates.js
@@ -7,5 +7,40 @@
 
 	How would you solve this problem if a temporary buffer is not allowed.
 */
-
 const SinglyLinkedList = require('./SinglyLinkedList');
+
+// Remove the singly linked list by keeping a map of values that have been seen
+// Time: O(N)
+// Space: O(d) where d is the number of unique values
+function removeDuplicates(rootNode) {
+	// Initialize a map between values and the frequency of those values
+	let valuesSeen = {};
+
+	// Start at the root node
+	let currNode = rootNode;
+	while (currNode !== null) {
+		// Add current node's value to frequency map
+		let currValue = currNode.value;
+		valuesSeen[currValue] = true;
+
+		// while the next node's value has been seen before, remove it
+		let nextNodeValue = currNode.next && currNode.next.value;
+		while (valuesSeen[nextNodeValue]) {
+			currNode.next = currNode.next.next;
+			nextNodeValue = currNode.next && currNode.next.value;
+		}
+		// Then go to the next node
+		currNode = currNode.next;
+	}
+}
+
+let linkedList = new SinglyLinkedList();
+linkedList.add(1);
+linkedList.add(2);
+linkedList.add(3);
+linkedList.add(2);
+linkedList.add(1);
+linkedList.add(1);
+console.log('Before remove duplicates:', linkedList.toArray());
+removeDuplicates(linkedList._head);
+console.log('After remove duplicates:', linkedList.toArray());
